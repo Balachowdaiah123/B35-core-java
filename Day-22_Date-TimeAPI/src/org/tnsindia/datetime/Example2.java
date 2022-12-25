bb1529e5265b19824163293ee945c655a8c0e1ec
Balachowdaiah-143 <114273609+Balachowdaiah-143@users.noreply.github.com>
2022-12-25 16:06:17 +0530

    first commit

---
+++ Day-22_Date-TimeAPI/src/org/tnsindia/datetime/Example2.java
@@ -0,0 +1,12 @@
+package org.tnsindia.datetime;
+import java.time.ZonedDateTime;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		ZonedDateTime n=ZonedDateTime.now();
+		System.out.println(n);
+
+	}
+
+}
